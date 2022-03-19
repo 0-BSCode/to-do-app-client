4248a9bdaf3f6f6b4301f5f22453475308f905ed
Bryan Sanchez <bryanipsanchez@gmail.com>
2022-03-19 22:34:01 +0800

    Planned rough structure of app

--- src/App.js
+++ src/App.js
@@ -7,3 +7,13 @@ function App() {
 }
 
 export default App;
+
+
+/*
+
+COMPONENT LAYERING
+- App (container)
+  - Header (title + toggle + add task)
+  - Body (task list + task actions + reorder line)
+  - Footer (attribution)
+*/
