dfe8d5968673e4e82d385b3a0672d8a10951f53f
Bryan Sanchez <bryanipsanchez@gmail.com>
2022-03-25 14:31:16 +0800

    Initiated actions

---
+++ src/actions/tasks.js
@@ -0,0 +1,20 @@
+export const Add = (task) => {
+    return {
+        type: 'ADD',
+        payload: task
+    }
+}
+
+export const Delete = (task) => {
+    return {
+        type: 'DELETE',
+        payload: task
+    }
+}
+
+export const Finished = (task) => {
+    return {
+        type: 'FINISHED',
+        payload: task
+    }
+}
