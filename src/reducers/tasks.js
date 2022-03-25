2936ba979b8a91088e5c45f12e1039db868ce9c7
Bryan Sanchez <bryanipsanchez@gmail.com>
2022-03-25 14:26:34 +0800

    Initialized reducer

--- src/reducers/tasks.js
+++ src/reducers/tasks.js
@@ -1,3 +1,12 @@
 export default (tasks=[], action) => {
-    
+    switch (action.type) {
+        case 'ADD':
+            return [...tasks, {name: action.payload, finished: false}];
+        case 'DELETE':
+            return tasks.filter(task => task.name != action.payload);
+        case 'FINISHED':
+            return tasks.map(task => task.name == action.payload? task.finished = true: '');
+        default:
+            return tasks;
+    }
 }
