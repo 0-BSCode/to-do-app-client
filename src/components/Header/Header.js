c331bdfaf981be3933b48c4078d830d5a4cd87e7
Bryan Sanchez <bryanipsanchez@gmail.com>
2022-03-26 18:17:57 +0800

    Set up basic toggle functionality

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,14 +2,37 @@ import React from 'react'
 import sunIcon from '../../images/icon-sun.svg';
 import moonIcon from '../../images/icon-moon.svg';
 import './styles.css'
+import {useState, useEffect} from 'react';
 
 const Header = () => {
+  const [light, setLight] = useState(true);
+  const toggleMode = (e) => {
+    e.preventDefault();
+    setLight(!light);
+  }
+
+  useEffect(() => {
+    const themeImg = document.querySelector(".theme-img");
+    const rootGetter = getComputedStyle(document.documentElement);
+    const rootSetter = document.documentElement.style;
+    console.log(rootGetter.getPropertyValue("--bright-blue"));
+    if (light) {
+      themeImg.setAttribute('src', `${moonIcon}`);
+      rootSetter.setProperty("--vl-gray", "white");
+    } else {
+      themeImg.setAttribute('src', `${sunIcon}`);
+      rootSetter.setProperty("--vl-gray", "black");
+    }
+  })
+
   return (
     <header className="header">
         <h1 className="heading">
             Todo
         </h1>
-        <button className="theme-btn">
+        <button 
+          className="theme-btn"
+          onClick={toggleMode}>
             <img
                 className="theme-img"
                 src={moonIcon}
