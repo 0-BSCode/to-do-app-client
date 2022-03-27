29ab35f6407b2c3fac802c226b701aab8c9cc272
Bryan Sanchez <bryanipsanchez@gmail.com>
2022-03-27 21:00:34 +0800

    Updated comments

--- src/App.js
+++ src/App.js
@@ -19,8 +19,11 @@ export default App;
 /*
 
 ISSUES:
-1. When filter aside from all = selected, clicking on task 
-doesn't reflect on display
-2. Beyond a certain string length, radio button deforms
-3. Hover over filters changes their color even once their selected
+1. Beyond a certain string length, radio button deforms
+2. Hover over filters changes their color even once their selected
+
+LEARNINGS:
+1. Color theme switching
+2. useEffect() alternative for updating variables
+
 */
