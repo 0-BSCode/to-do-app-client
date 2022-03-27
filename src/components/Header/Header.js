2f5413e1336d807f0ffd8d3b31e457c0b01e6586
Bryan Sanchez <bryanipsanchez@gmail.com>
2022-03-27 21:43:04 +0800

    Enabled color switch for more elements

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -22,16 +22,20 @@ const Header = () => {
       rootSetter.setProperty("--bg-color", rootGetter.getPropertyValue("--vl-gray"));
       rootSetter.setProperty("--card-bg-color", "white");
       rootSetter.setProperty("--task-text-color", rootGetter.getPropertyValue("--vd-gray-blue"));
+      rootSetter.setProperty("--taskinfo-text-color", rootGetter.getPropertyValue("--d-gray-blue"));
       rootSetter.setProperty("--text-hover-color", "black");
       rootSetter.setProperty("--border-color", rootGetter.getPropertyValue("--l-gray-blue"));
+      rootSetter.setProperty("--text-finished-color", rootGetter.getPropertyValue("--l-gray-blue"));
     } else {
       themeImg.setAttribute('src', `${sunIcon}`);
       rootSetter.setProperty("--bg-image", rootGetter.getPropertyValue("--bg-image-dark"));
       rootSetter.setProperty("--bg-color", rootGetter.getPropertyValue("--vd-blue"));
       rootSetter.setProperty("--card-bg-color", rootGetter.getPropertyValue("--vd-desat-blue"));
       rootSetter.setProperty("--task-text-color", rootGetter.getPropertyValue("--l-gray-blue_dark"));
+      rootSetter.setProperty("--taskinfo-text-color", rootGetter.getPropertyValue("--d-gray-blue_dark"));
       rootSetter.setProperty("--text-hover-color", "white");
       rootSetter.setProperty("--border-color", rootGetter.getPropertyValue("--vd-gray-blue_dark2"));
+      rootSetter.setProperty("--text-finished-color", rootGetter.getPropertyValue("--vd-gray-blue_dark"));
     }
   })
 
