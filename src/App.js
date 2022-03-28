d12e465e9f1dc00297cbefb5b3f8844507a0e6b6
Bryan Sanchez <bryanipsanchez@gmail.com>
2022-03-28 21:18:37 +0800

    Updated comments

--- src/App.js
+++ src/App.js
@@ -19,11 +19,11 @@ export default App;
 /*
 
 ISSUES:
-1. Drag and drop functionality
-2. Responsive design for desktop
+1. Responsive design for desktop
 
 LEARNINGS:
 1. Color theme switching
 2. useEffect() alternative for updating variables
+3. Drag and drop elements
 
 */
