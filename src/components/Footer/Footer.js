3051cf9ce2cb518c67e67cd2f3d39093ec3be412
Bryan Sanchez <bryanipsanchez@gmail.com>
2022-03-29 11:28:41 +0800

    Added GitHub link

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="attribution">
       Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-      Coded by <a href="#">Bryan Sanchez</a>.
+      Coded by <a href="https://github.com/Bry-cmd">Bryan Sanchez</a>.
     </footer>
   )
 }
