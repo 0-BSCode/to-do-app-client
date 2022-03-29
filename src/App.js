e09176d37e5472c97401c45d9f29fef1b15c7218
Bryan Sanchez <bryanipsanchez@gmail.com>
2022-03-29 21:25:28 +0800

    Removed comments

--- src/App.js
+++ src/App.js
@@ -14,18 +14,3 @@ function App() {
 }
 
 export default App;
-
-
-/*
-
-TODO
-1. Box shadow around card
-
-LEARNINGS:
-1. Color theme switching
-2. useEffect() alternative for updating variables
-3. Drag and drop elements
-4. Organizing React files
-5. Redux
-
-*/
