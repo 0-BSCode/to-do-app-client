194645166bec02e6b09db797e40a9c5dc472a214
Bryan Sanchez <bryanipsanchez@gmail.com>
2022-03-29 13:08:58 +0800

    Initialized constants

--- src/actions/tasks.js
+++ src/actions/tasks.js
@@ -1,20 +1,22 @@
+import {ADD, DELETE, FINISHED} from '../constants/actionTypes';
+
 export const Add = (task) => {
     return {
-        type: 'ADD',
+        type: ADD,
         payload: task
     }
 }
 
 export const Delete = (task) => {
     return {
-        type: 'DELETE',
+        type: DELETE,
         payload: task
     }
 }
 
 export const Finished = (task) => {
     return {
-        type: 'FINISHED',
+        type: FINISHED,
         payload: task
     }
 }
--- src/reducers/tasks.js
+++ src/reducers/tasks.js
@@ -1,10 +1,38 @@
-export default (state=[], action) => {
+import {ADD, DELETE, FINISHED} from '../constants/actionTypes';
+const initialTasks = [
+    {
+        name: 'Complete online JavaScript course',
+        finished: true
+    },
+    {
+        name: 'Jog around the park 3x',
+        finished: false
+    },
+    {
+        name: '10 minutes meditation',
+        finished: false
+    },
+    {
+        name: 'Read for 1 hour',
+        finished: false
+    },
+    {
+        name: 'Pick up groceries',
+        finished: false
+    },
+    {
+        name: 'Complete Todo App on Frontend Mentor',
+        finished: false
+    }
+];
+
+export default (state=[...initialTasks], action) => {
     switch (action.type) {
-        case 'ADD':
+        case ADD:
             return [...state, {name: action.payload, finished: false}];
-        case 'DELETE':
+        case DELETE:
             return state.filter(task => task.name != action.payload);
-        case 'FINISHED':
+        case FINISHED:
             return state.map(task => task.name == action.payload? {name: task.name, finished: !task.finished}: task);
         default:
             return state;
